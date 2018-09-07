020bb5268a3750dbd0f2859bf8a10c20ce371570
Kacper Sawicki <kacper_sawicki@sevenet.pl>
2018-09-07 08:24:24 +0200

    Initial commit

---
+++ src/main/java/pl/sevenet/zsl/ZslApplication.java
@@ -0,0 +1,12 @@
+package pl.sevenet.zsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZslApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/sevenet/zsl/ZslApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.sevenet.zsl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZslApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
